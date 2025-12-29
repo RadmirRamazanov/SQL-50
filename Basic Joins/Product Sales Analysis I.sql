a263de30960e75faf9e6ade788c24cedfe68d560
Рамазанов Радмир <radmir.cool007@gmail.com>
2025-12-29 23:23:24 +0400

    add files

---
+++ Basic Joins/Product Sales Analysis I.sql
@@ -0,0 +1,7 @@
+-- MySQL, Oracle SQL & PostgreSQL
+SELECT 
+    Product.product_name, Sales.year, Sales.price 
+FROM 
+    Sales
+INNER JOIN Product
+    ON Sales.product_id = Product.product_id
---
+++ Basic Joins/Replace Employee ID With The Unique Identifier.sql
@@ -0,0 +1,7 @@
+-- MySQL, Oracle SQL & PostgreSQL
+SELECT 
+    EmployeeUNI.unique_id, Employees.name 
+FROM 
+    Employees
+LEFT JOIN EmployeeUNI
+    ON Employees.id = EmployeeUNI.id
